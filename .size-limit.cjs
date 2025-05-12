ee59b80f783fb2c8a8e595710a78c5a635116731
Adam Fahy <apfahy@gmail.com>
2025-05-12 14:22:52 -0400

    chore: updated size-limit and build config to match project params

--- .size-limit.cjs
+++ .size-limit.cjs
@@ -1,10 +1,10 @@
 module.exports = [
   {
-    path: "dist/index.js",
-    limit: "1 KB"
+    path: 'dist/index.js',
+    limit: '2 KB',
   },
   {
-    path: "dist/index.mjs",
-    limit: "1 KB"
-  }
+    path: 'dist/index.cjs',
+    limit: '2 KB',
+  },
 ];
